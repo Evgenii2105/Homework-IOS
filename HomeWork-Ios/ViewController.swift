ca64944b465734e182a0c6228a2873a9fabdac04
Evgenii2105 <Fomev1@yandex.ru>
2024-09-03 23:43:23 +0300

    HW1IOS

--- HomeWork-Ios/ViewController.swift
+++ HomeWork-Ios/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Создал первый Label
+    private let lastNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Fomichev"
+        label.font = .systemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // Создал второй Label
+    private var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Evgenii Vadimovich"
+        label.font = .systemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // Создаем контейнерный UIView с фиксированными размерами
+    private var labelsContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
-
-
+    
+    func setupViews() {
+        view.addSubview(labelsContainerView)
+        labelsContainerView.addSubview(lastNameLabel)
+        labelsContainerView.addSubview(fullNameLabel)
+        
+        // Настройка Auto Layout для контейнерного UIView и Label's
+        
+        NSLayoutConstraint.activate  ([
+            labelsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            labelsContainerView.widthAnchor.constraint(equalToConstant: 200),
+            labelsContainerView.heightAnchor.constraint(equalToConstant: 100),
+            
+            lastNameLabel.leadingAnchor.constraint(equalTo: labelsContainerView.leadingAnchor, constant: 10),
+            lastNameLabel.centerYAnchor.constraint(equalTo: labelsContainerView.centerYAnchor),
+            lastNameLabel.trailingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor, constant: -10),
+            
+            fullNameLabel.trailingAnchor.constraint(equalTo: labelsContainerView.trailingAnchor, constant: -10),
+            fullNameLabel.centerYAnchor.constraint(equalTo: labelsContainerView.centerYAnchor)
+        ])
+        
+        // Добавляем настройки для предотвращения перекрытия
+        lastNameLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        fullNameLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+    }
 }
 
