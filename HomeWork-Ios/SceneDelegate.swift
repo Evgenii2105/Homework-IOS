37bceb6e3fa418a8bae2148833a0840291a74b2d
Evgenii2105 <Fomev1@yandex.ru>
2024-09-06 01:05:14 +0300

    HW3IOSButton

--- HomeWork-Ios/SceneDelegate.swift
+++ HomeWork-Ios/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TwoLabelsViewController()
+        window?.rootViewController = GuestCounterViewController()
         window?.makeKeyAndVisible()
         
     }
